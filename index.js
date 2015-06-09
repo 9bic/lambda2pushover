0ab6b79419784f1e06a0208bed5c165455b4686f
tks_saito <tks_saito@nhk-mt.co.jp>
2015-06-09 11:25:57 +0900

    it work

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-var pushover = require('node-pushover');
+var Pushover = require('pushover-notifications');
 var credential = require('./credential.json');
 
 exports.handler = function(event, context) {
-  var pushover = new push({
+  var pushover = new Pushover({
     user: credential.user,
     token: credential.token
   });
-    var snsContent = event.Records[0].Sns.Message;
-    var msg = {
-      message: snsContent,
-      title: "SNS2Pushover",
-    };
-    pushover.send(msg, function(err, result) {
-      if (err) {
-        context.fail(err);
-      }
-      context.succeed(result);
-  });
+
+  var snsSubject = event.Records[0].Sns.Subject;
+  var snsContent = event.Records[0].Sns.Message;
+  var msg = {
+    message: snsContent,
+    title: snsSubject,
+  };
+
+  pushover.send(msg, function(err, res) {
+    console.log(err);
+    console.log(res);
+    context.done(err, res);
+  }); 
 };
